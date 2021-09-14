0695d3dce1cb6a75ac087590f920d32737340fca
muscatelli <muscatelli92@gmail.com>
2021-09-14 01:32:55 +0000

    modelando

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -16,21 +16,22 @@ class User(Base):
     lastname = Column(String, nullable=False)
     email = Column(String, nullable=False)
     post= relationship('post')
+    followers = relationship('followers')
 
 class Followers(Base):
     __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey("user.id"))
     user_to_id = Column(Integer, ForeignKey("user.id"))
-    user = relationship(User)
+   
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String, nullable=False)
     autor_id = Column(Integer, ForeignKey("user.id"))
-    user_id = Column(Integer, ForeignKey("post.id") )
-   # post_id = Column(Integer, ForeignKey("user.id"))
+    user_id = Column(Integer, ForeignKey("post.id"))
+    user = relationship('User')
     #comment = relationship(User)
 
 class Post(Base):
@@ -42,6 +43,10 @@ class Post(Base):
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
+    media_type= Column(Enum("video", "image"))
+    url= Column(String(200))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('post')
  
 
     def to_dict(self):
